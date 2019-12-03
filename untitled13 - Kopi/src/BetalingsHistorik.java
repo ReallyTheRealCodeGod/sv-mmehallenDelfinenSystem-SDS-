d378df4865f09451139ad1b2a2d21e2a915881aa
Deji <ehinlanwo.deji@live.dk>
2019-12-03 12:51:38 +0100

    hvad er betalings historik

---
+++ untitled13 - Kopi/src/BetalingsHistorik.java
@@ -0,0 +1,29 @@
+ import java.time.LocalDate;
+
+    public class BetalingsHistorik{
+        private double pris;
+        private LocalDate betalingsDato;
+        private String bankNummer;
+
+        BetalingsHistorik(double pris, LocalDate betalingsDato, String bankNummer){
+            this.pris = pris;
+            this.betalingsDato = betalingsDato;
+            this.bankNummer = bankNummer;
+        }
+
+        public double getPris(){
+            return pris;
+        }
+
+        public LocalDate getBetalingsDato(){
+            return betalingsDato;
+        }
+
+        public String bankNummer(){
+            return bankNummer;
+        }
+
+        public String toString(){
+            return "[" +pris +", "+ betalingsDato +", "+ bankNummer + "]";
+        }
+    }
