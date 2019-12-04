90e9e2ac70f22ef61262726dec711c0b088e690c
Deji <ehinlanwo.deji@live.dk>
2019-12-04 11:21:13 +0100

    opret medlem addered til ui
    Signed-off-by: Deji <ehinlanwo.deji@live.dk>

--- untitled13 - Kopi/src/UserInterface.java
+++ untitled13 - Kopi/src/UserInterface.java
@@ -1,22 +1,33 @@
-import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
 
 public class UserInterface {
     Stage stage;
 
     UserInterface(Stage stage){
         this.stage = stage;
+        stage.setTitle("Svømmeklubben System");
     }
 
-    public void updateStage(Scene scene){
-
-        stage.setTitle("Svømmeklubben System");
+    public void updateStage(Parent root){
+        Scene scene = new Scene(root, 700, 500);
         stage.setScene(scene); // ændre scene størrelse
         stage.show();
     }
@@ -40,6 +51,8 @@ public class UserInterface {
         grid.add(visMedlemKasser, 0, 0);
         grid.add(logUdKasser, 0, 1);
         grid.setStyle("-fx-background-color: ALICEBLUE;");
+
+        updateStage(grid);
     }
 
     public void formandStartSide() {
@@ -65,5 +78,158 @@ public class UserInterface {
         grid1.add(visMedlemFormand, 2, 0);
         grid1.add(logUd, 1, 1);
         grid1.setStyle("-fx-background-color: ALICEBLUE;");
+        updateStage(grid1);
     }
-}
+
+    public void OpretMedlemForm() throws Exception {
+
+        //Setting size of window
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        double stagesizex = primScreenBounds.getWidth() / 2;
+        double stagesizey = primScreenBounds.getHeight() / 1.5;
+
+
+            //Labels og fields
+        Text nameLabel = new Text("Navn");
+        TextField nameText = new TextField();
+
+        Text ageLabel = new Text("Fødselsdato");
+
+        DatePicker datepicker = new DatePicker();
+        datepicker.setPrefSize(270, 20);
+
+        Text adresseLabel = new Text("Adresse");
+        TextField adresseText = new TextField();
+
+        Text nummerLabel = new Text("Husnummer");
+        TextField nummerText = new TextField();
+
+        Text postNummerLabel = new Text("Postnummer");
+        TextField postNummerText = new TextField();
+
+            Text emailLabel = new Text("E-Mail");
+            TextField emailText = new TextField();
+
+            //Køn label og en gruppe for denne
+            Text genderLabel = new Text("Køn");
+
+            ToggleGroup groupGender = new ToggleGroup();
+            RadioButton maleRadio = new RadioButton("Mand");
+            maleRadio.setToggleGroup(groupGender);
+            RadioButton femaleRadio = new RadioButton("Kvinde");
+            femaleRadio.setToggleGroup(groupGender);
+            RadioButton otherRadio = new RadioButton("Andet");
+            otherRadio.setToggleGroup(groupGender);
+
+            //hbox til køn
+            HBox hboxGender = new HBox();
+            hboxGender.setSpacing(10);
+            hboxGender.getChildren().addAll(maleRadio, femaleRadio, otherRadio);
+
+            Text medlemstypeLabel = new Text("Medlemstype");
+
+            //Choice box til drop down
+            ChoiceBox medlemsTypeBox = new ChoiceBox();
+            medlemsTypeBox.getItems().addAll("Aktiv", "Passiv");
+            medlemsTypeBox.setPrefWidth(270);
+
+
+            Text aktivitetsTypeLabel = new Text("Aktivitetstype");
+
+            //Choice box til drop down
+            ChoiceBox aktivitetsTypeBox = new ChoiceBox();
+            aktivitetsTypeBox.getItems().addAll("Konkurrence", "Motionist");
+            aktivitetsTypeBox.setPrefWidth(270);
+            //knapper
+            Button buttonGem = new Button("Gem");
+            Button buttonAflys = new Button("Aflys");
+            buttonGem.setPrefSize(180, 20);
+            buttonAflys.setPrefSize(80, 20);
+            //hbox til knapper
+            HBox hboxKnap = new HBox();
+            hboxKnap.setSpacing(10);
+            hboxKnap.getChildren().addAll(buttonGem, buttonAflys);
+            hboxKnap.setAlignment(Pos.BOTTOM_RIGHT);
+
+            //Date formatter
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+
+            //Make button do stuff
+            buttonGem.setOnAction((event ->
+                    System.out.println(nameText.getText() +
+                            datepicker.getValue() +
+                            ((RadioButton) groupGender.getSelectedToggle()).getText()+
+                            (adresseText.getText()+" "+nummerText.getText())+
+                            emailText.getText() +
+                            aktivitetsTypeBox.getValue() +
+                            medlemsTypeBox.getValue()
+                    )));
+
+            //Make method go to formand side
+            buttonAflys.setOnAction((event -> System.out.println("Go to formand page")));
+
+            //laver gridpane
+            GridPane gridPane = new GridPane();
+            gridPane.setMinSize(stagesizex / 2, stagesizey);
+
+            //padding
+            gridPane.setPadding(new Insets(50));
+
+            //Setting the vertical and horizontal gaps between the columns
+            gridPane.setVgap(25);
+            gridPane.setHgap(40);
+
+            //Setting the Grid alignment
+            gridPane.setAlignment(Pos.TOP_LEFT);
+
+            //nodes i grid
+            gridPane.add(nameLabel, 0, 0);
+            gridPane.add(nameText, 1, 0);
+
+            gridPane.add(ageLabel, 0, 1);
+            gridPane.add(datepicker, 1, 1);
+
+            gridPane.add(adresseLabel, 0, 2);
+            gridPane.add(adresseText, 1, 2);
+
+            gridPane.add(nummerLabel, 0, 3);
+            gridPane.add(nummerText, 1, 3);
+
+            gridPane.add(postNummerLabel, 0, 4);
+            gridPane.add(postNummerText, 1, 4);
+
+            gridPane.add(emailLabel, 0, 5);
+            gridPane.add(emailText, 1, 5);
+
+            gridPane.add(genderLabel, 0, 6);
+            gridPane.add(hboxGender, 1, 6);
+
+            gridPane.add(medlemstypeLabel, 0, 7);
+            gridPane.add(medlemsTypeBox, 1, 7);
+
+            gridPane.add(aktivitetsTypeLabel, 0, 8);
+            gridPane.add(aktivitetsTypeBox, 1, 8);
+
+            gridPane.add(hboxKnap, 1, 10);
+
+            gridPane.setGridLinesVisible(false);
+            gridPane.setMaxSize(stagesizex / 2, stagesizey);
+
+            //imporeterer billede
+            InputStream logo = new URL("https://i.imgur.com/jb8srK2.png%22").openStream();
+            Image image = new Image(logo);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(stagesizex / 2);
+            imageView.setFitHeight(stagesizey);
+            imageView.setPreserveRatio(false);
+
+            //laver ekstra gridpane
+            GridPane bigGrid = new GridPane();
+            bigGrid.add(gridPane, 0, 0);
+            bigGrid.add(imageView, 1, 0);
+
+            bigGrid.getStylesheets().add("sample/CSS.css");
+            updateStage(bigGrid);
+        }
+    }
+
